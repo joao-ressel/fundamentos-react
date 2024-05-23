19bb51157a04d5fad74fe77065ba4bb149a66b2d
joao-ressel <joao.resselti@gmail.com>
2024-05-23 15:20:52 -0500

    refact: alterando componente Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,52 @@
+import { Avatar } from "./Avatar";
+import { Comment } from "./Comment";
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar hasBorder src="https://github.com/diego3g.png" />
+          <div className={styles.authorInfo}>
+            <strong>Diogo Fernandes</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="23 de de maio às 07:44h" dateTime="2024-05-23 07:44:12">
+          Publicado há 1h
+        </time>
+        {/* permite que passe o atributo para passar o horario exato */}
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉<a href="#"> jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto </a>
+          <a href="#">#nlw </a>
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+    </article>
+  );
+}
