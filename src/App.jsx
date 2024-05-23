a698d13a5617a73a24aa9ae7859a4c543ebe6b08
joao-ressel <joao.resselti@gmail.com>
2024-05-23 14:26:42 -0500

    feat: adicionando o componente Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Post } from "./Post";
+import { Post } from "./components/Post";
 import { Header } from "./components/Header";
 import "./global.css";
 import styles from "./App.module.css";
@@ -11,14 +11,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Diego Fernandes"
-            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, aperiam obcaecati itaque tempore inventore ullam velit, commodi doloremque ipsum, voluptatem cumque hic natus blanditiis vero illo maxime asperiores sint rem."
-          />
-          <Post
-            author="Maria da Silva"
-            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, aperiam obcaecati itaque tempore inventore ullam velit, commodi doloremque ipsum, voluptatem cumque hic natus blanditiis vero illo maxime asperiores sint rem."
-          />
+        <Post/>
+        <Post/>
         </main>
       </div>
     </>
