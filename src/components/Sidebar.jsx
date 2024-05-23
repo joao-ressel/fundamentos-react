3f67d9fd2f1f8a4b7e97ce271e0fcff3193eada5
joao-ressel <joao.resselti@gmail.com>
2024-05-23 08:36:15 -0500

    feat: adicionando estilo ao sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,27 @@
+import styles from "./Sidebar.module.css";
+import { PencilLine } from "@phosphor-icons/react";
 export function Sidebar() {
-  return <aside>sidebar</aside>;
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1575089976121-8ed7b2a54265?q=80&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/joao-ressel.png"
+        />
+        <strong>João Carlos</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
 }
