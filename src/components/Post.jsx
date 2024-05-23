fd5a7c4a2849922429dca8f3369a9b4feac6f5f2
joao-ressel <joao.resselti@gmail.com>
2024-05-23 16:43:54 -0500

    refact: iterando no JSX e alterando propriedades do post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,38 +1,62 @@
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
-export function Post() {
+export function Post({ author, content, publishedAt }) {
+  // const publishedDateFormatted = new Intl.DateTimeFormat('pt-BR',{
+  //   day:'2-digit',
+  //   month: 'long',
+  //   hour:'2-digit',
+  //   minute:'2-digit',
+  // }).format(publishedAt);
+
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    {
+      locale: ptBR,
+    }
+  );
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar hasBorder src="https://github.com/diego3g.png" />
+          <Avatar hasBorder src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Diogo Fernandes</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
-        <time title="23 de de maio às 07:44h" dateTime="2024-05-23 07:44:12">
-          Publicado há 1h
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
         </time>
         {/* permite que passe o atributo para passar o horario exato */}
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          👉<a href="#"> jane.design/doctorcare</a>
-        </p>
-        <p>
-          <a href="#">#novoprojeto </a>
-          <a href="#">#nlw </a>
-          <a href="#">#rocketseat</a>
-        </p>
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p>{line.content}</p>;
+          } else if (line.type === "link") {
+            return (
+              <p>
+                <a href="#">{line.content}</a>
+              </p>
+            );
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
