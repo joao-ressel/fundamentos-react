c838e09d86cf2907f8afe0c580e8fcc2638c798a
joao-ressel <joao.resselti@gmail.com>
2024-05-24 10:26:03 -0500

    feat: adicionando o contador de likes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,18 @@
 import { ThumbsUp, Trash } from "@phosphor-icons/react";
 import styles from "./Comment.module.css";
+import { useState } from "react";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handlelikeComment() {
+    setLikeCount((state) =>{
+      return state + 1
+    });
   }
 
   return (
@@ -28,9 +37,8 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
-            {" "}
-            <ThumbsUp /> Aplaudir <span>20</span>{" "}
+          <button onClick={handlelikeComment}>
+            <ThumbsUp /> Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
